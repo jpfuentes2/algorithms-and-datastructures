9e3c2fad5ea06c4763914fd7bd3b74852900da67
Jacques Fuentes <jpfuentes2@gmail.com>
2016-05-06 12:11:15 -0700

    Use piecewise notation for min/max functions

--- util/util.go
+++ util/util.go
@@ -1,16 +1,22 @@
 package datastructures
 
 // Max gives the max of a vs b
+//
+// Max(a,b) =  | a if a > b
+//             | b otherwise
 func Max(a, b int) int {
-	if a >= b {
+	if a > b {
 		return a
 	}
 	return b
 }
 
 // Min gives the min of a vs b
+//
+// Min(a,b) =  | a if a < b
+//             | b otherwise
 func Min(a, b int) int {
-	if a <= b {
+	if a < b {
 		return a
 	}
 	return b
