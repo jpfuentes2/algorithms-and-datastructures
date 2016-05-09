6e978ac24d15c0048e6a8d3520d20d8cc070d4ef
Jacques Fuentes <jpfuentes2@gmail.com>
2016-05-08 18:25:51 -0700

    Add thread-safety tests for stack/queue

--- datastructures/queue_test.go
+++ datastructures/queue_test.go
@@ -1,8 +1,11 @@
 package datastructures
 
 import (
+	"sort"
+	"sync"
 	"testing"
 
+	"github.com/jpfuentes2/algorithms-and-datastructures/util"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -51,4 +54,50 @@ func TestErrEmptyQueue(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Equal(t, ErrEmptyQueue, err)
+}
+
+func TestQueueThreadSafety(t *testing.T) {
+	queue := NewQueue()
+	ints := util.Range(0, 50)
+	wg := sync.WaitGroup{}
+	wg.Add(len(ints))
+
+	for _, i := range ints {
+		go func(x int) {
+			queue.Enqueue(x)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	assert.False(t, queue.IsEmpty())
+	assert.Equal(t, len(ints), queue.Len())
+
+	wg = sync.WaitGroup{}
+	wg.Add(len(ints))
+	actual := struct {
+		ints []int
+		*sync.Mutex
+	}{make([]int, 0, len(ints)), &sync.Mutex{}}
+
+	for i := 0; i <= len(ints); i++ {
+		go func() {
+			if e, err := queue.Dequeue(); err == nil {
+				actual.Lock()
+				actual.ints = append(actual.ints, (e).(int))
+				actual.Unlock()
+				wg.Done()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	sort.Ints(ints)
+	sort.Ints(actual.ints)
+
+	assert.True(t, queue.IsEmpty())
+	assert.Equal(t, 0, queue.Len())
+	assert.Equal(t, ints, actual.ints)
 }
--- datastructures/stack_test.go
+++ datastructures/stack_test.go
@@ -1,8 +1,11 @@
 package datastructures
 
 import (
+	"sort"
+	"sync"
 	"testing"
 
+	"github.com/jpfuentes2/algorithms-and-datastructures/util"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -42,4 +45,50 @@ func TestErrEmptyStack(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Equal(t, ErrEmptyStack, err)
+}
+
+func TestStackThreadSafety(t *testing.T) {
+	stack := NewStack()
+	ints := util.Range(0, 50)
+	wg := sync.WaitGroup{}
+	wg.Add(len(ints))
+
+	for _, i := range ints {
+		go func(x int) {
+			stack.Push(x)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	assert.False(t, stack.IsEmpty())
+	assert.Equal(t, len(ints), stack.Len())
+
+	wg = sync.WaitGroup{}
+	wg.Add(len(ints))
+	actual := struct {
+		ints []int
+		*sync.Mutex
+	}{make([]int, 0, len(ints)), &sync.Mutex{}}
+
+	for i := 0; i <= len(ints); i++ {
+		go func() {
+			if e, err := stack.Pop(); err == nil {
+				actual.Lock()
+				actual.ints = append(actual.ints, (e).(int))
+				actual.Unlock()
+				wg.Done()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	sort.Ints(ints)
+	sort.Ints(actual.ints)
+
+	assert.True(t, stack.IsEmpty())
+	assert.Equal(t, 0, stack.Len())
+	assert.Equal(t, ints, actual.ints)
 }
